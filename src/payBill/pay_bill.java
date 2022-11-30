faf2dbdc348fce96bb53da7806b82ca9879c0331
Jasleen Sondhi <jasleensondhi@gmail.com>
2022-11-30 18:55:52 +0530

    Add files via upload

---
+++ src/payBill/pay_bill.java
@@ -0,0 +1,29 @@
+package payBill;
+
+import java.awt.*;
+import javax.swing.*;
+
+public class pay_bill extends JFrame{
+    public pay_bill(){
+        JEditorPane j = new JEditorPane();
+        j.setEditable(false);
+        //String jurl="https://paytm.com/electricity-bill-payment";
+        try {
+            j.setPage("http://www.google.com");
+        }catch (Exception e) {
+            j.setContentType("text/html");
+            j.setText("<html>Could not load</html>");
+        }
+
+        JScrollPane scrollPane = new JScrollPane(j);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        getContentPane().add(scrollPane);
+        setPreferredSize(new Dimension(800,600));
+        setSize(800,800);
+        setLocation(250,120);
+        setVisible(true);
+    }
+    public static void main(String[] args){
+        new pay_bill().setVisible(true);
+    }
+}
